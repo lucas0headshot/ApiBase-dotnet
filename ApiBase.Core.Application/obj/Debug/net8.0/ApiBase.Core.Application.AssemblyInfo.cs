62d35b52dcfbf6e30662702fff69bbf590409e47
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-07-27 17:50:26 -0300

    removendo camada Core do projeto

--- ApiBase.Core.Application/obj/Debug/net8.0/ApiBase.Core.Application.AssemblyInfo.cs
+++ ApiBase.Core.Application/obj/Debug/net8.0/ApiBase.Core.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiBase.Core.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12b392dafdeadae1fdc4565db42895bb1e1797ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6c2ddbc132e969952f55a4f2e4799d274313fdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiBase.Core.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiBase.Core.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
